77b84167212f99acc0b8daf4324e95ab9af466fd
Luciano Frizzera <lucaju@me.com>
2022-07-15 13:52:28 -0400

    fix: 🐛 add try/catch blocks to avoid script interruptions

--- crawler/scraper/watch.ts
+++ crawler/scraper/watch.ts
@@ -19,6 +19,9 @@ export const watchPage = async ({ browser, branches, ytId, timezone }: IWatchPag
   const metadata = await getMetadata(page);
   if (!metadata) return;
 
+  const stats = await getStats(page);
+  if (!stats) return;
+
   const recommendations: IRecommendedVideo[] = await getRecommendations(page, branches);
 
   const collectedAt = DateTime.now().setZone(timezone).toBSON();
@@ -26,6 +29,7 @@ export const watchPage = async ({ browser, branches, ytId, timezone }: IWatchPag
   const video: IVideo = {
     ytId,
     ...metadata,
+    ...stats,
     recommendations,
     collectedAt,
     recommended: 1,
@@ -37,41 +41,19 @@ export const watchPage = async ({ browser, branches, ytId, timezone }: IWatchPag
 };
 
 const getMetadata = async (page: Page) => {
-  // * Metadata headers
-  const watchContent = await page.$('#watch7-content');
-  if (!watchContent) return;
+  try {
+    // * Metadata headers
+    await page.waitForSelector('#watch7-content');
 
-  const metas: { [x: string]: any } = await watchContent.$$eval('meta', (content) => {
-    const metaObject = {};
+    const watchContent = await page.$('#watch7-content');
+    if (!watchContent) return;
 
-    content.forEach((meta) => {
-      const key = meta.getAttribute('itemprop');
-      const value = meta.getAttribute('content');
-      if (!key || !value) return;
-
-      //@ts-ignore
-      metaObject[key] = value;
-    });
-
-    return metaObject;
-  });
-
-  const title = metas.name;
-  const description = metas.description;
-  const paid = metas.paid === 'True' ? true : false;
-  const channelId = metas.channelId;
-  const duration = metas.duration;
-  const uploadDate = metas.uploadDate;
-  const datePublished = metas.datePublished;
-
-  const channelMeta: { [x: string]: any } = await watchContent.$$eval(
-    'span[itemprop="author"] > link',
-    (content) => {
+    const metas: { [x: string]: any } = await watchContent.$$eval('meta', (content) => {
       const metaObject = {};
 
       content.forEach((meta) => {
         const key = meta.getAttribute('itemprop');
-        const value = meta.getAttribute('content') || meta.getAttribute('href');
+        const value = meta.getAttribute('content');
         if (!key || !value) return;
 
         //@ts-ignore
@@ -79,94 +61,145 @@ const getMetadata = async (page: Page) => {
       });
 
       return metaObject;
-    }
-  );
-  const channelName = channelMeta.name ?? '';
+    });
 
-  await page.waitForSelector('#info-contents');
+    const title = metas.name;
+    const description = metas.description;
+    const paid = metas.paid === 'True' ? true : false;
+    const channelId = metas.channelId;
+    const duration = metas.duration;
+    const uploadDate = metas.uploadDate;
+    const datePublished = metas.datePublished;
 
-  // * Info Contents
+    const channelMeta: { [x: string]: any } = await watchContent.$$eval(
+      'span[itemprop="author"] > link',
+      (content) => {
+        const metaObject = {};
 
-  const container = await page.$('#info-contents');
-  if (!container) return;
+        content.forEach((meta) => {
+          const key = meta.getAttribute('itemprop');
+          const value = meta.getAttribute('content') || meta.getAttribute('href');
+          if (!key || !value) return;
 
-  // * Views
-  const viewCount = await container
-    .$eval(
-      'div > #count > ytd-video-view-count-renderer > .view-count',
+          //@ts-ignore
+          metaObject[key] = value;
+        });
+
+        return metaObject;
+      }
+    );
+    const channelName = channelMeta.name ?? '';
+
+    // * Results
+    const videoDetails: Partial<IVideo> = {
+      title,
+      description,
+      duration,
+      uploadDate,
+      datePublished,
+      channel: {
+        id: channelId,
+        name: channelName,
+      },
+      paid,
+    };
+
+    return videoDetails;
+  } catch (error) {
+    console.log(`!! ERROR: ${error}`);
+    return;
+  }
+};
+
+const getStats = async (page: Page) => {
+  try {
+    await page.waitForSelector('#info-contents');
+
+    // * Info Contents
+
+    const container = await page.$('#info-contents');
+    if (!container) return;
+
+    // * Views
+    const viewCount = await container
+      .$eval(
+        'div > #count > ytd-video-view-count-renderer > .view-count',
+        (content) => content.innerHTML
+      )
+      .catch(() => '');
+    const views = cleanCount(viewCount);
+
+    // * Hashtags
+    const hastags = await container.$$eval('div > .super-title > a', (content) =>
+      content.map((n) => n.innerHTML)
+    );
+
+    const interactButtons = await container.$$('div > ytd-toggle-button-renderer');
+    // first button: Like
+    // second button: dislike
+
+    // * Likes Exact
+    //aria-label contains the exact number.
+    const likesExactCount = await interactButtons?.[0]?.$eval('yt-formatted-string', (content) =>
+      content.getAttribute('aria-label')
+    );
+    const likes = likesExactCount ? cleanCount(likesExactCount) : -1;
+
+    // * Comments
+    const commentsAvailable = await page
+      .waitForSelector('ytd-comments-header-renderer', { timeout: 5000 })
+      .catch(() => null);
+    const commentsContainer = commentsAvailable
+      ? await page.$('ytd-comments-header-renderer')
+      : null;
+
+    const commentsCount = await commentsContainer?.$eval(
+      'div > h2 > yt-formatted-string > span',
       (content) => content.innerHTML
-    )
-    .catch(() => '');
-  const views = cleanCount(viewCount);
+    );
 
-  // * Hashtags
-  const hastags = await container.$$eval('div > .super-title > a', (content) =>
-    content.map((n) => n.innerHTML)
-  );
+    const comments = commentsCount ? cleanCount(commentsCount) : -1;
 
-  const interactButtons = await container.$$('div > ytd-toggle-button-renderer');
-  // first button: Like
-  // second button: dislike
+    const adCompanion = await getAdCompanion(page);
 
-  // * Likes Exact
-  //aria-label contains the exact number.
-  const likesExactCount = await interactButtons?.[0]?.$eval('yt-formatted-string', (content) =>
-    content.getAttribute('aria-label')
-  );
-  const likes = likesExactCount ? cleanCount(likesExactCount) : -1;
+    // * Results
+    const videoDetails: Partial<IVideo> = {
+      hastags,
+      views,
+      likes,
+      comments,
+      adCompanion,
+    };
 
-  // * Comments
-  const commentsAvailable = await page
-    .waitForSelector('ytd-comments-header-renderer', { timeout: 5000 })
-    .catch(() => null);
-  const commentsContainer = commentsAvailable ? await page.$('ytd-comments-header-renderer') : null;
-
-  const commentsCount = await commentsContainer?.$eval(
-    'div > h2 > yt-formatted-string > span',
-    (content) => content.innerHTML
-  );
-
-  const comments = commentsCount ? cleanCount(commentsCount) : -1;
-
-  const adCompanion = await getAdCompanion(page);
-
-  // * Results
-  const videoDetails: Partial<IVideo> = {
-    title,
-    description,
-    duration,
-    hastags,
-    uploadDate,
-    datePublished,
-    channel: {
-      id: channelId,
-      name: channelName,
-    },
-    paid,
-    views,
-    likes,
-    comments,
-    adCompanion,
-  };
-
-  return videoDetails;
+    return videoDetails;
+  } catch (error) {
+    console.log(`!! ERROR: ${error}`);
+    return;
+  }
 };
 
 const getAdCompanion = async (page: Page) => {
-  const container = await page.$('#companion');
-  if (!container) return;
+  try {
+    const container = await page.$('#companion');
+    if (!container) return;
 
-  const block = await page.$('#block > div#text');
-  if (!block) return;
+    const block = await page.$('#block > div#text');
+    if (!block) return;
 
-  const title = await block.$eval('div#header', (content) => content.innerHTML.trim());
-  const domain = await block.$eval('div#desc > span#domain', (content) => content.innerHTML.trim());
+    const title = await block.$eval('div#header', (content) => content.innerHTML.trim());
+    const domain = await block.$eval('div#desc > span#domain', (content) =>
+      content.innerHTML.trim()
+    );
 
-  if (!title && !domain) return;
+    if (!title && !domain) return;
 
-  const adCompanion: IAdCompanion = { title, domain };
+    const adCompanion: IAdCompanion = { title, domain };
 
-  return adCompanion;
+    return adCompanion;
+  } catch (error) {
+    console.log(`!! ERROR: ${error}`);
+    return;
+  }
 };
 
 const getRecommendations = async (page: Page, branches?: number) => {
@@ -199,10 +232,18 @@ const getRecommendedVideoDetails = async (item: ElementHandle<Element>) => {
   const ytId = link.toString().split('=')[1];
 
   // * Title
-  const title = await item.$eval('#video-title', (content) => content.innerHTML.trim());
+  let title: string | undefined;
+
+  try {
+    title = await item.$eval('#video-title', (content) => content.innerHTML.trim());
+  } catch {
+    title = undefined;
+  }
 
   // * Results
-  const recommendedVideo: IRecommendedVideo = { ytId, title };
+  const recommendedVideo: IRecommendedVideo = { ytId };
+  if (title) recommendedVideo.title = title;
+
   return recommendedVideo;
 };
 
