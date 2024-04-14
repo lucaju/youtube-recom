4c32047de6658c0050a37e9d21e9527dfabdd838
Luciano Frizzera <lucaju@me.com>
2024-04-14 17:58:02 -0400

    fix(site): 🐛 remove unused route

--- apps/site/app/api/auth/jwt/route.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from '@/auth';
-import { NextResponse } from 'next/server';
-
-export async function GET(req: NextAuthRequest) {
-  const token = await auth(req);
-  return NextResponse.json(token);
-}
