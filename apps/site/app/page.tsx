a0f135f88ce67b920860038b481e6768f3f5f23c
Luciano Frizzera <lucaju@me.com>
2024-04-14 17:59:38 -0400

    feat(site): 🎸 redirect users from homepage to dashboard if they are in session

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,9 +1,15 @@
+import { auth } from '@/auth';
 import Authentication from '@/components/authentiation';
-// import { ModeToggle } from '@/components/ui/themeToggle';
 import { TypographyH1, TypographyH2 } from '@/components/ui/typography';
+import { redirect } from 'next/navigation';
 import { LuGitCompare } from 'react-icons/lu';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  if (session) {
+    redirect('/dashboard/projects');
+  }
+
   return (
     <main className="flex h-3/4 min-h-screen flex-col items-center justify-center">
       <div className="relative flex flex-col place-items-center space-y-6">
@@ -12,7 +18,6 @@ export default function Home() {
             YouTube
             <br />
             Recommendation
-            {/* <ModeToggle /> */}
           </TypographyH1>
           <div className="flex items-center space-x-2">
             <LuGitCompare className="mb-2 h-8 w-8" />
